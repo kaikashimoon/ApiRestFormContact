14527924dc96b878d65d801646983ae7f1d0c6c9
kai <leonc4667@gmail.com>
2022-06-22 13:39:22 -0400

    make it better

---
+++ index.js
@@ -0,0 +1,66 @@
+require("dotenv").config();
+const express = require("express");
+const nodeMail = require("nodemailer");
+const path = require("path");
+const cors = require("cors");
+const { resolve } = require("path");
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+  }));
+
+async function mainMail(name, email, number, text) {
+  const transporter = await nodeMail.createTransport({
+    service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
+        clientId: process.env.OAUTH_CLIENTID,
+        clientSecret: process.env.OAUTH_CLIENT_SECRET,
+        refreshToken: process.env.OAUTH_REFRESH_TOKEN
+      }
+  });
+  const mailOption = {
+    from: process.env.MAIL_USERNAME,
+    to: process.env.MAIL_USERNAME,
+    html: `<b> You got a message from  <br />
+    Email : ${email} <br />
+    Name: ${name} <br />
+    text: ${text} <br />
+    Number: ${number} <b/>`,
+  };
+  try {
+    await transporter.sendMail(mailOption);
+    return Promise.resolve("Message Sent Successfully!");
+  } catch (error) {
+    console.log(error)
+    return Promise.reject(error);
+  }
+}
+
+app.post("/v1/contact", async (req, res, next) => {
+  const { name, email, number, text} = req.body;
+  try {
+    const data = await mainMail(name, email, number, text);
+    console.log(data)
+    res.status(200).json({message: "Message send successfully!"});
+  } catch (error) {
+    return res.status(500).json({ message: error });  }
+});
+
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
+
